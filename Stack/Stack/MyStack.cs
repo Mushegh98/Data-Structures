6ca7de4b156b2c1255d4f1a5f80be658b4ddeac0
Mushegh98 <53486603+Mushegh98@users.noreply.github.com>
2019-11-10 02:01:05 +0400

    Add files via upload

---
+++ Stack/Stack/MyStack.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Stack
+{
+    /// <summary>
+    /// Stack предстовляет коллекцию одинакого типа , и работает как LIFO(Last In First Out)
+    /// </summary>
+    class MyStack
+    {
+            object[] array;
+            private static int size;
+            private int count = 0;
+            private int top = -1;
+        
+            /// <summary>
+            /// Инициализирует новый экземпляр класса, который является пустым и имеет начальную емкость по умолчанию
+            /// </summary>
+            public MyStack()
+            {
+                size = 1000;
+                array = new object[size];
+            }
+            
+            /// <summary>
+            /// Инициализирует новый экземпляр класса ,который является пустым и имеет начальную емкость по переданному аргументу
+            /// </summary>
+            /// <param name="SizeStack"></param>
+            public MyStack(int SizeStack)
+            {
+                size = SizeStack;
+                array = new object[size];
+            }
+            
+            /// <summary>
+            /// Количество элементов Стека
+            /// </summary>
+            public int Count
+            {
+                get { return count; }
+            }
+            /// <summary>
+            /// Полностю очищает Стек
+            /// </summary>
+            public void Clear()
+            {
+                top = -1;
+                count = 0;
+            }
+            /// <summary>
+            /// Удаляет последный элемент Стека
+            /// </summary>
+            public void Pop()
+            {
+                if (top != -1)
+                {
+                    array[top--] = null;
+                    count--;
+                }
+                else throw new IndexOutOfRangeException();
+            }
+            /// <summary>
+            /// Добавляет element в Стек
+            /// </summary>
+            /// <param name="element"></param>
+            public void Push(object element)
+            {
+                if (top != array.Length - 1)
+                {
+                    array[++top] = element;
+                    count++;
+                }
+                else throw new IndexOutOfRangeException();
+            }
+            
+            /// <summary>
+            /// Возвращает последный элемент Стека
+            /// </summary>
+            /// <returns></returns>
+            public object Top()
+            {
+                if (top >= 0)
+                {
+                    return array[top];
+                }
+                else throw new IndexOutOfRangeException();
+            }
+        
+    }
+
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack stack = new MyStack();
+            stack.Push(1);
+            stack.Push(5);
+            stack.Push(8);
+            Console.WriteLine($"Count={stack.Count}");
+            stack.Pop();
+            stack.Push(5);
+            stack.Push(8);
+            stack.Clear();
+            stack.Push(14);
+            stack.Push(5);
+            stack.Push(8);
+            stack.Push(5);
+            stack.Push(")");
+            stack.Push(")");
+            stack.Push(8);
+            stack.Pop();
+            stack.Pop();
+            Console.WriteLine($"Top={stack.Top()}");
+            stack.Pop();
+            
+        }
+    }
+}
